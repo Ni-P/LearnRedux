b7eb83f3dc8ff8a5f120a5d22b123323fd8c5d0f
Niko Kristian Pinnis <niko.pinnis@outlook.com>
2017-08-31 20:52:10 +0300

    Add action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -7,57 +7,9 @@ var stateDefault = {
   hobbies: [],
   movies: []
 };
-var nextHobbyId = 1;
-var nextMovieId = 1;
 
-var oldReducer = (state = stateDefault, action) => {
-  // console.log('New action', action);
-
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      };
-    case 'ADD_HOBBY':
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            hobby: action.hobby
-          }
-        ]
-      };
-    case 'REMOVE_HOBBY':
-      return {
-        ...state,
-        hobbies: state.hobbies.filter(hobby => hobby.id !== action.id)
-      };
-    case 'ADD_MOVIE':
-      return {
-        ...state,
-        movies: [
-          ...state.movies,
-          {
-            id: nextMovieId++,
-            title: action.title,
-            genre: action.genre
-          }
-        ]
-      };
-    case 'REMOVE_MOVIE':
-      return {
-        ...state,
-        movies: state.movies.filter(movie => movie.id !== action.id)
-      };
-
-    default:
-      return state;
-  }
-};
-
+// Name reducer and action generators
+// ------------------------------------------------------------
 var nameReducer = (state = 'Anonymous', action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -67,6 +19,16 @@ var nameReducer = (state = 'Anonymous', action) => {
   }
 };
 
+var changeName = name => {
+  return {
+    type: 'CHANGE_NAME',
+    name
+  };
+};
+
+// Hobbies reducer and action generators
+// ------------------------------------------------------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_HOBBY':
@@ -84,6 +46,23 @@ var hobbiesReducer = (state = [], action) => {
   }
 };
 
+var addHobby = hobby => {
+  return {
+    type: 'ADD_HOBBY',
+    hobby
+  };
+};
+
+var removeHobby = id => {
+  return {
+    type: 'REMOVE_HOBBY',
+    id
+  };
+};
+
+// Movies reducer and action generators
+// ------------------------------------------------------------
+var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_MOVIE':
@@ -103,12 +82,31 @@ var moviesReducer = (state = [], action) => {
   }
 };
 
+var addMovie = (title, genre) => {
+  return {
+    type: 'ADD_MOVIE',
+    title,
+    genre
+  };
+};
+
+var removeMovie = id => {
+  return {
+    type: 'REMOVE_MOVIE',
+    id
+  };
+};
+// End of reducers
+// ------------------------------------------------------------
+
+// Combine all reducers
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
   movies: moviesReducer
 });
 
+// Create our store
 var store = redux.createStore(
   reducer,
   redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
@@ -126,48 +124,62 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState ', currentState);
 
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Niko'
-});
+store.dispatch(changeName('Niko'));
 
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Running'
-});
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Walking'
-});
-store.dispatch({
-  type: 'REMOVE_HOBBY',
-  id: 2
-});
+// store.dispatch({
+//   type: 'CHANGE_NAME',
+//   name: 'Niko'
+// });
 
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Kristian'
-});
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'Running'
+// });
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'Walking'
+// });
+// store.dispatch({
+//   type: 'REMOVE_HOBBY',
+//   id: 2
+// });
 
-store.dispatch({
-  type: 'ADD_MOVIE',
-  title: 'Boring Cinema',
-  genre: 'Boredom'
-});
-store.dispatch({
-  type: 'ADD_MOVIE',
-  title: 'PG-18',
-  genre: 'Family'
-});
-store.dispatch({
-  type: 'ADD_MOVIE',
-  title: 'Kill many things',
-  genre: 'Action'
-});
+store.dispatch(addHobby('Running'));
+store.dispatch(addHobby('Walking'));
+store.dispatch(removeHobby(2));
 
-store.dispatch({
-  type: 'REMOVE_MOVIE',
-  id: 1
-});
+store.dispatch(changeName('Kristian'));
+
+// store.dispatch({
+//   type: 'CHANGE_NAME',
+//   name: 'Kristian'
+// });
+
+// store.dispatch({
+//   type: 'ADD_MOVIE',
+//   title: 'Boring Cinema',
+//   genre: 'Boredom'
+// });
+// store.dispatch({
+//   type: 'ADD_MOVIE',
+//   title: 'PG-18',
+//   genre: 'Family'
+// });
+// store.dispatch({
+//   type: 'ADD_MOVIE',
+//   title: 'Kill many things',
+//   genre: 'Action'
+// });
+
+store.dispatch(addMovie('Boring Cinema', 'Boredom'));
+store.dispatch(addMovie('PG-18', 'Family'));
+store.dispatch(addMovie('Kill many things', 'Action'));
+
+// store.dispatch({
+//   type: 'REMOVE_MOVIE',
+//   id: 1
+// });
+
+store.dispatch(removeMovie(1));
 
 // console.log('Name should be Niko', store.getState());
