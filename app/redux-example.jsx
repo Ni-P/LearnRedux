5f95d116cf6c4f8364b18493124a42aa912b317b
Niko Kristian Pinnis <niko.pinnis@outlook.com>
2017-08-31 18:48:27 +0300

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,29 @@ const redux = require('redux');
 console.log('redux example');
 
 var reducer = (state = { name: 'no name' }, action) => {
-  return state;
+  // console.log('New action', action);
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState ', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Niko'
+};
+
+store.dispatch(action);
+
+console.log('Name should be Niko', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,9 +9,27 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 console.log('currentState ', store.getState());
+
+var action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'new searchText'
+};
+
+store.dispatch(action);
+
+console.log('Should have new searchText', store.getState());
